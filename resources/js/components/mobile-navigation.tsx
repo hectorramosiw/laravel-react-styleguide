dc27c8fbe3bbfd1e1e1181f575b6f743482185da
Hector Ramos <hector.ramos@iconwireless.ph>
2026-03-04 12:46:15 +0800

    feat: add desktop and mobile navigation to Main Header

---
+++ resources/js/components/mobile-navigation.tsx
@@ -0,0 +1,13 @@
+export default function MobileNavigation() {
+    return (
+        <nav className="bg-gray-800 text-white py-4">
+            <div className="container mx-auto px-4">
+                <ul className="flex space-x-4">
+                    <li><a href="/" className="hover:text-blue-300">Home</a></li>
+                    <li><a href="/about" className="hover:text-blue-300">About</a></li>
+                    <li><a href="/contact" className="hover:text-blue-300">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
