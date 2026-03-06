72bb966cc8f261096947441a75d77f67bf605b85
Hector Ramos <hector.ramos@iconwireless.ph>
2026-03-06 10:20:27 +0800

    feat: fix formatting of eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
 
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier/flat';
 import importPlugin from 'eslint-plugin-import';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import storybook from "eslint-plugin-storybook";
 import globals from 'globals';
 import typescript from 'typescript-eslint';
 
